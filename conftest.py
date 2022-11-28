d90e06387d91e019f28a2ef0ccbf88c1ea67b51a
Rammsat <saturn9911@mail.ru>
2022-11-28 11:35:28 +0300

    Поправлены замечания по тесту и фикстуре

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
 import pytest
 from selene.support.shared import browser
-from selene import be
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def open_browser():
     browser.config.window_width = 1400
     browser.config.window_height = 800
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
--- homework_test.py
+++ homework_test.py
@@ -1,10 +1,12 @@
 from selene.support.shared import browser
-from selene import have
+from selene import have, be
 
 
 def test_positive(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     assert browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in'))
 
 
 def test_negative(open_browser):
-    assert browser.element('[id="search"]').should(have.text('qwe'))
+    browser.element('[name="q"]').should(be.blank).type('gdfjkgdjflkglkdfjg').press_enter()
+    assert browser.element('[id="topstuff"]').should(have.text('По запросу gdfjkgdjflkglkdfjg ничего не найдено.'))
